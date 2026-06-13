6df4a621ec47c209dc1dfecaf6e3801591719d1f
jj <71215065+junnjiee@users.noreply.github.com>
2026-06-14 02:23:59 +0800

    added library

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,7 +8,7 @@ export default async function sitemap() {
     lastModified: post.metadata.publishedAt,
   }))
 
-  let routes = ['', '/blog', '/side-quests'].map((route) => ({
+  let routes = ['', '/blog', '/library', '/side-quests'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
