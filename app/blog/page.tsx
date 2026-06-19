b74443b3a93f05dece7d2516b40d3fb801b27742
jj <71215065+junnjiee@users.noreply.github.com>
2026-06-20 00:07:43 +0800

    remove this section

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,26 +1,5 @@
 import { BlogPosts } from "app/components/posts";
 
-type LibraryItem = {
-  name: string;
-  description: string;
-  href: string;
-};
-
-const libraryItems: LibraryItem[] = [
-  {
-    name: "Commonplace",
-    description:
-      "A curation of interesting links on the internet worth keeping.",
-    href: "https://junnjiee.notion.site/commonplace",
-  },
-  {
-    name: "Compendium",
-    description:
-      "Collection of notes, information, and knowledge related to my interests.",
-    href: "https://junnjiee-compendium.vercel.app/",
-  },
-];
-
 export const metadata = {
   title: "Blog",
   description: "junnjiee's blog.",
@@ -34,29 +13,6 @@ export default function Page() {
           My Blog
         </h1>
         <BlogPosts />
-      </div>
-
-      <h1 className="mb-8 text-2xl font-medium tracking-tight">
-        Digital Garden
-      </h1>
-      <div className="space-y-5">
-        {libraryItems.map((item) => (
-          <article key={item.name}>
-            <h2 className="font-medium tracking-tight text-neutral-900 dark:text-neutral-100">
-              <a
-                href={item.href}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="title-link"
-              >
-                {item.name}
-              </a>
-            </h2>
-            <p className="mt-2 leading-relaxed text-description">
-              {item.description}
-            </p>
-          </article>
-        ))}
       </div>
     </section>
   );
